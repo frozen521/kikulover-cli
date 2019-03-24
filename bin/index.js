73c79ff2f07009fd82eadd5864b610aad46c8b60
copyer1998 <2782644531@qq.com>
2019-03-24 13:46:25 +0800

    12

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,11 @@
-console.log('my first cli');
+
+process.title = 'kk';
+
+require('commander')
+    .version(require('../package').version)
+    .usage('<command> [options]')
+    .command('generate', 'generate file from a template (short-cut alias: "g")')
+    .parse(process.argv)
+
+
+require('./kk-generate');
